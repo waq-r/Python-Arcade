221edf3f44f97c2ab912db8661bba0ed19481e9e
waqar3 <ahmed@waqar.org>
2023-12-18 18:38:03 +0000

    9-mex function solution

--- 09 Mex Function/mex-function.py
+++ 09 Mex Function/mex-function.py
@@ -1,1 +1,11 @@
 # Mex Function
+def solution(s, upperBound):
+    found = -1
+    for i in range(upperBound):
+        if not i in s:
+            found = i
+            break
+    else:
+        found = upperBound
+
+    return found
