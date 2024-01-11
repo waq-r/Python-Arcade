6eef40b7bc3c5e81999c0eda5e7748633f9bf066
waqar3 <ahmed@waqar.org>
2024-01-11 14:59:37 +0000

    29 is test solvable

--- 29-Is_Test_Solvable/is-test-solvable.py
+++ 29-Is_Test_Solvable/is-test-solvable.py
@@ -1,1 +1,8 @@
 # Is Test Solvable
+def solution(ids, k):
+    digitSum = lambda n: 0 if n == 0 else (n % 10) + digitSum(int(n / 10))
+
+    sm = 0
+    for questionId in ids:
+        sm += digitSum(questionId)
+    return sm % k == 0
