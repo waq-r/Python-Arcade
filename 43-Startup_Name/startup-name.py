0457438f86709128d8b725de2661e050e488c239
Waqar Ahmed <ahmed@waqar.org>
2024-02-14 15:58:56 +0000

    43 startup name solution

--- 43-Startup_Name/startup-name.py
+++ 43-Startup_Name/startup-name.py
@@ -1,1 +1,7 @@
 # Startup Name
+def solution(companies):
+    cmp1 = set(companies[0])
+    cmp2 = set(companies[1])
+    cmp3 = set(companies[2])
+    res = ((cmp1 & cmp2) | (cmp2 & cmp3) | (cmp1 & cmp3)) ^ (cmp1 & cmp2 & cmp3)
+    return list(sorted(list(res)))
