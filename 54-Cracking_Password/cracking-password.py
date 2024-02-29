f81c0cc0a372647dd23844c79a6693a2b9370397
Waqar Ahmed <ahmed@waqar.org>
2024-02-29 16:26:35 +0000

    54 craking password solution

--- 54-Cracking_Password/cracking-password.py
+++ 54-Cracking_Password/cracking-password.py
@@ -1,1 +1,8 @@
 # Cracking Password
+from itertools import product
+
+def solution(digits, k, d):
+    def createNumber(digs):
+        return "".join(map(str, digs))
+
+    return [createNumber(x) for x in filter(lambda e: int(createNumber(e)) % d == 0 , list(sorted(product(digits, repeat=k))))]
