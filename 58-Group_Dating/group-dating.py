e2db5734df2a8868e6e38009d8191cefa04d65f3
Waqar Ahmed <ahmed@waqar.org>
2024-03-06 15:46:14 +0000

    58 group dating solution

--- 58-Group_Dating/group-dating.py
+++ 58-Group_Dating/group-dating.py
@@ -1,1 +1,3 @@
 # Group Dating
+def solution(male, female):
+    return [[male[x] for x in range(len(male)) if male[x] != female[x]], [female[x] for x in range(len(female)) if male[x] != female[x]]]
