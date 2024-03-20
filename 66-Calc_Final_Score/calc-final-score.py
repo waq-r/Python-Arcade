0b3ebcd70eab3cb88c6f90e286aab3dd0fcf3082
Waqar Ahmed <ahmed@waqar.org>
2024-03-20 15:52:41 +0000

    66 calc final score solution

--- 66-Calc_Final_Score/calc-final-score.py
+++ 66-Calc_Final_Score/calc-final-score.py
@@ -1,1 +1,12 @@
 # Calc Final Score
+def solution(scores, n):
+    gen = iter([x**2 for x in sorted(scores, reverse=True)])
+
+    res = 0
+    try:
+        for _ in range(n):
+            res += next(gen)
+    except StopIteration:
+        res //= 5
+
+    return res
